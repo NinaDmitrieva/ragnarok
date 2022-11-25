9ec4e081916c85bfbd71b36bdb9de6b7a33f8819
Nina Dmitrieva <nina.dmitrieva.spb@yandex.ru>
2022-11-25 14:33:37 +0300

    логика открытия меню в хедере, плавный скролл по секциям

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -1,3 +1,29 @@
 import '../styles/reset.scss';
 import '../styles/styles.scss';
 import '../styles/mixins.scss';
+
+const classes = {
+    opened: 'opened'
+}
+
+const header = document.querySelector('.header');
+const menuBtn = document.querySelector('.header-menu__btn');
+const menuLink = document.querySelectorAll('.menu-link');
+
+
+const toggleMenu = () => {header.classList.toggle(classes.opened)};
+const scrollToSection = (e) =>{
+    e.preventDefaauit();
+    const href = e.currentTarget.getAttribute('href');
+
+    if(!href && !href.startsWith('#')) return;
+
+    const section = href.slice(1);
+    const top = document.getElementById(section)?.offsetTop || 0;
+    window.scrollTo({top, behavior: 'smooth'})
+}
+
+
+
+menuBtn.addEventListener('click', toggleMenu);
+menuLink.forEach((link) => link.addEventListener('click', scrollToSection));
