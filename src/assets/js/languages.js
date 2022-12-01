954c872fca3be5163b85575396dd5868e6b6d4cf
Nina Dmitrieva <nina.dmitrieva.spb@yandex.ru>
2022-12-02 00:38:59 +0300

    перевод секций

--- src/assets/js/languages.js
+++ src/assets/js/languages.js
@@ -1,18 +1,48 @@
 export const languages = {
     en:{
+        //header
         editions: 'Editions',
         controller: 'Controller',
         about: 'About',
         explore: 'Explore',
         news: 'News',
         fag: 'FAQ',
+        release: 'Release',
+        title:'God of War Ragnarök',
+        discriptionSubtitle: 'Taking place three years following the events of the previous game, Fimbulwinter, a great winter that spans three summers, is drawing to a close which will begin the prophesied Ragnarök.',
+        discriptionBtn: 'buy now',
+
+        //editions
+        editionsTitle: 'Standart Editions',
+        editionsItemStandartFirst: 'Offline play enabled',
+        editionsItemStandartSecond: 'Remote Play supported',
+        editionsItemStandartLast: 'Trigger effect required',
+        editionsTitleDeluxe:'Digital Deluxe Edition',
+        editionsItemDeluxeFirst:'All features of Standard Edition',
+        editionsItemDeluxeSecond: 'Darkdale Armour',
+        editionsItemDeluxetLast: 'Official digital soundtrack',
     },
     ru: {
+        //header
         editions: 'Издания',
         controller: 'Джостик',
         about: 'Об Игре',
         explore: 'Исследовать',
         news: 'Новости',
         fag: 'Помощь',
+        release: 'До выпуска',
+        title: 'Бог войны Рагнарёк',
+        discriptionSubtitle: 'События, происходящие через три года после событий предыдущей игры, Fimbulwinter, великая зима, которая продлилась три лета, подходит к концу, что положит начало предсказанному Рагнареку',
+        discriptionBtn: 'купить',
+
+        //editions
+        editionsTitle: 'Стандартные издания',
+        editionsItemStandartFirst: 'Offline play enabled',
+        editionsItemStandartSecond: 'Remote Play supported',
+        editionsItemStandartLast: 'Trigger effect required',
+        editionsTitleDeluxe: 'Эксклюзивное издание',
+        editionsItemDeluxeFirst: 'All features of Standard Edition',
+        editionsItemDeluxeSecond: 'Darkdale Armour',
+        editionsItemDeluxetLast: 'Official digital soundtrack',
     },
 };
