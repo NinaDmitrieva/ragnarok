ecf37dcd0339a8c691c8621f46e58f3c26dae244
Nina Dmitrieva <nina.dmitrieva.spb@yandex.ru>
2022-12-02 16:33:44 +0300

    перевод секций

--- src/assets/js/languages.js
+++ src/assets/js/languages.js
@@ -1,14 +1,14 @@
 export const languages = {
-    en:{
+    en: {
         //header
         editions: 'Editions',
         controller: 'Controller',
         about: 'About',
         explore: 'Explore',
         news: 'News',
-        fag: 'FAQ',
+        faq: 'FAQ',
         release: 'Release',
-        title:'God of War Ragnarök',
+        title: 'God of War Ragnarök',
         discriptionSubtitle: 'Taking place three years following the events of the previous game, Fimbulwinter, a great winter that spans three summers, is drawing to a close which will begin the prophesied Ragnarök.',
         discriptionBtn: 'buy now',
 
@@ -17,10 +17,51 @@ export const languages = {
         editionsItemStandartFirst: 'Offline play enabled',
         editionsItemStandartSecond: 'Remote Play supported',
         editionsItemStandartLast: 'Trigger effect required',
-        editionsTitleDeluxe:'Digital Deluxe Edition',
-        editionsItemDeluxeFirst:'All features of Standard Edition',
+        editionsTitleDeluxe: 'Digital Deluxe Edition',
+        editionsItemDeluxeFirst: 'All features of Standard Edition',
         editionsItemDeluxeSecond: 'Darkdale Armour',
         editionsItemDeluxetLast: 'Official digital soundtrack',
+
+        //controller
+        controllerTitle: 'DualSense',
+        controllerText: 'You can also buy an exclusive controller with the theme for PS5',
+        controllerOffer: 'If you purchase a controller and a game(any edition), you will receive a discount on two items',
+        controllerBuyBtn: 'buy dualsense with a game',
+
+        //video
+        sectionTitle: 'About Game',
+        videoTitle: 'Watch the Trailer',
+        videoBtn: 'play',
+
+        //explore
+        exploreTitle: 'Explore the God of War series',
+        exploreswitchMinimum: 'Minimum',
+        exploreswitchRecommended: 'Recommended',
+        exploreswitchStandart: 'Standart',
+        exploreswitchLimited: 'Limited',
+
+        //news
+        newsItemTitle: 'will receive a large update on release',
+        newsItemsubtitle: 'God of War: Ragnarok is fast approaching.No wonder that we have been regularly receiving new information and materials related to the title in recent times.Last week, a trailer presenting the possibilities of the game on PlayStation 5 hit the network, and a little earlier we learned that the work on the production was completed .Meanwhile, it turns out that the upcoming sequel to the adventures of Kratos and Atreus will receive a major update on release.',
+        newsBtn: 'read',
+        newsItemTitleSecond: 'is officially one of the largest PS4 games by size',
+        newsItemsubtitleSecond: 'Digital distribution enthusiasts need to get ready for tons of downloads.The second adventure of Kratos and Atreus in the High North from the premiere will occupy nearly 120GB - whendeciding to buy a title from PlayStation Store, it is worth planning your download wisely to start the game on the day of the premiere.',
+        newsItemTitleThird: 'is the final chapter in an epic story. See the new video',
+        newsItemSubtitleThird: ' Familiar characters will return to the game, but also completely new ones.We are talking here about Thor, presented as a brutal and mentally unstable performer of "dirty work" in the services of Odin, as well as the ruler of the gods himself, who left his mark on the events of the previousinstallment, although he did not appear in it personally.',
+        newsItemTitleFourth: 'tells a mature story',
+        newsItemSubtitleFourth: '    The highly anticipated game God of War: Ragnarok will debut in just over twenty days.On this occasion, representatives of the Santa Monica studio together with Sony decided to release a new trailer dedicated to the epic history of Kratos and Atreus - in this way we have received confirmation that Ragnarok is the final chapter of the adventures of the god of war.',
+
+        //FAQ
+        faqItem:'When is the next God of War coming out?',
+        faqItemContent: 'God of War Ragnarök will release on November 9, 2022.To get the latest updates on release timing, make sure you follow Santa Monica Studio on Twitter, Instagram and Facebook!',
+        faqItemSecond: 'Will God of War Ragnarök have an upgrade path?',
+        faqItemContentSecond: 'Yes)',
+        faqItemThird: 'What is god of war adout?',
+        faqItemContentThird: 'First era (Greek mythology) God of War was first released in North America on March 22, 2005, for the PlayStation 2. After ten years in the service of the Olympian gods, Spartan soldier Kratos is tasked by Athena to find Pandora s box, the key to defeating Ares, the God of War, who is running amok through Athens.',
+
+        //footer
+        footerInfo: 'Download of this product is subject to the PlayStation Network Terms of Service and our Software Usage Terms plus any specific additional conditions applying to this product.If you do not wish to accept these terms, do not download this product.See Terms of Service for more important information.',
+
     },
     ru: {
         //header
@@ -29,7 +70,7 @@ export const languages = {
         about: 'Об Игре',
         explore: 'Исследовать',
         news: 'Новости',
-        fag: 'Помощь',
+        faq: 'Помощь',
         release: 'До выпуска',
         title: 'Бог войны Рагнарёк',
         discriptionSubtitle: 'События, происходящие через три года после событий предыдущей игры, Fimbulwinter, великая зима, которая продлилась три лета, подходит к концу, что положит начало предсказанному Рагнареку',
@@ -44,5 +85,46 @@ export const languages = {
         editionsItemDeluxeFirst: 'All features of Standard Edition',
         editionsItemDeluxeSecond: 'Darkdale Armour',
         editionsItemDeluxetLast: 'Official digital soundtrack',
+
+        //controller
+        controllerTitle: 'DualSense',
+        controllerText: 'You can also buy an exclusive controller with the theme for PS5',
+        controllerOffer: 'If you purchase a controller and a game(any edition), you will receive a discount on two items',
+        controllerBuyBtn: 'buy dualsense with a game',
+
+        //video
+        sectionTitle: 'About Game',
+        videoTitle: 'Watch the Trailer',
+        videoBtn: 'play',
+
+        //explore
+        exploreTitle: 'Explore the God of War series',
+        exploreswitchMinimum: 'Minimum',
+        exploreswitchRecommended: 'Recommended',
+        exploreswitchStandart: 'Standart',
+        exploreswitchLimited: 'Limited',
+
+        //news
+        newsItemTitle: 'will receive a large update on release',
+        newsItemsubtitle: 'God of War: Ragnarok is fast approaching.No wonder that we have been regularly receiving new information and materials related to the title in recent times.Last week, a trailer presenting the possibilities of the game on PlayStation 5 hit the network, and a little earlier we learned that the work on the production was completed .Meanwhile, it turns out that the upcoming sequel to the adventures of Kratos and Atreus will receive a major update on release.',
+        newsBtn: 'read',
+        newsItemTitleSecond: 'is officially one of the largest PS4 games by size',
+        newsItemsubtitleSecond: 'Digital distribution enthusiasts need to get ready for tons of downloads.The second adventure of Kratos and Atreus in the High North from the premiere will occupy nearly 120GB - whendeciding to buy a title from PlayStation Store, it is worth planning your download wisely to start the game on the day of the premiere.',
+        newsItemTitleThird: 'is the final chapter in an epic story. See the new video',
+        newsItemSubtitleThird: ' Familiar characters will return to the game, but also completely new ones.We are talking here about Thor, presented as a brutal and mentally unstable performer of "dirty work" in the services of Odin, as well as the ruler of the gods himself, who left his mark on the events of the previousinstallment, although he did not appear in it personally.',
+        newsItemTitleFourth: 'tells a mature story',
+        newsItemSubtitleFourth: '    The highly anticipated game God of War: Ragnarok will debut in just over twenty days.On this occasion, representatives of the Santa Monica studio together with Sony decided to release a new trailer dedicated to the epic history of Kratos and Atreus - in this way we have received confirmation that Ragnarok is the final chapter of the adventures of the god of war.',
+
+        //FAQ
+        faqItem: 'When is the next God of War coming out?',
+        faqItemContent: 'God of War Ragnarök will release on November 9, 2022.To get the latest updates on release timing, make sure you follow Santa Monica Studio on Twitter, Instagram and Facebook!',
+        faqItemSecond: 'Will God of War Ragnarök have an upgrade path?',
+        faqItemContentSecond: 'Yes)',
+        faqItemThird: 'What is god of war adout?',
+        faqItemContentThird: 'First era (Greek mythology) God of War was first released in North America on March 22, 2005, for the PlayStation 2. After ten years in the service of the Olympian gods, Spartan soldier Kratos is tasked by Athena to find Pandora s box, the key to defeating Ares, the God of War, who is running amok through Athens.',
+
+        //footer
+        footerInfo: 'Download of this product is subject to the PlayStation Network Terms of Service and our Software Usage Terms plus any specific additional conditions applying to this product.If you do not wish to accept these terms, do not download this product.See Terms of Service for more important information.',
+
     },
 };
