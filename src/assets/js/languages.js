95398875561f2a416174134f31ed3ca781c08fd5
Nina Dmitrieva <nina.dmitrieva.spb@yandex.ru>
2022-12-03 00:04:39 +0300

    перевод

--- src/assets/js/languages.js
+++ src/assets/js/languages.js
@@ -35,70 +35,7 @@ export const languages = {
 
         //explore
         exploreTitle: 'Explore the God of War series',
-        exploreswitchMinimum: 'Minimum',
-        exploreswitchRecommended: 'Recommended',
-        exploreswitchStandart: 'Standart',
-        exploreswitchLimited: 'Limited',
-
-        //news
-        newsItemTitle: 'will receive a large update on release',
-        newsItemsubtitle: 'God of War: Ragnarok is fast approaching.No wonder that we have been regularly receiving new information and materials related to the title in recent times.Last week, a trailer presenting the possibilities of the game on PlayStation 5 hit the network, and a little earlier we learned that the work on the production was completed .Meanwhile, it turns out that the upcoming sequel to the adventures of Kratos and Atreus will receive a major update on release.',
-        newsBtn: 'read',
-        newsItemTitleSecond: 'is officially one of the largest PS4 games by size',
-        newsItemsubtitleSecond: 'Digital distribution enthusiasts need to get ready for tons of downloads.The second adventure of Kratos and Atreus in the High North from the premiere will occupy nearly 120GB - whendeciding to buy a title from PlayStation Store, it is worth planning your download wisely to start the game on the day of the premiere.',
-        newsItemTitleThird: 'is the final chapter in an epic story. See the new video',
-        newsItemSubtitleThird: ' Familiar characters will return to the game, but also completely new ones.We are talking here about Thor, presented as a brutal and mentally unstable performer of "dirty work" in the services of Odin, as well as the ruler of the gods himself, who left his mark on the events of the previousinstallment, although he did not appear in it personally.',
-        newsItemTitleFourth: 'tells a mature story',
-        newsItemSubtitleFourth: '    The highly anticipated game God of War: Ragnarok will debut in just over twenty days.On this occasion, representatives of the Santa Monica studio together with Sony decided to release a new trailer dedicated to the epic history of Kratos and Atreus - in this way we have received confirmation that Ragnarok is the final chapter of the adventures of the god of war.',
-
-        //FAQ
-        faqItem:'When is the next God of War coming out?',
-        faqItemContent: 'God of War Ragnarök will release on November 9, 2022.To get the latest updates on release timing, make sure you follow Santa Monica Studio on Twitter, Instagram and Facebook!',
-        faqItemSecond: 'Will God of War Ragnarök have an upgrade path?',
-        faqItemContentSecond: 'Yes)',
-        faqItemThird: 'What is god of war adout?',
-        faqItemContentThird: 'First era (Greek mythology) God of War was first released in North America on March 22, 2005, for the PlayStation 2. After ten years in the service of the Olympian gods, Spartan soldier Kratos is tasked by Athena to find Pandora s box, the key to defeating Ares, the God of War, who is running amok through Athens.',
-
-        //footer
-        footerInfo: 'Download of this product is subject to the PlayStation Network Terms of Service and our Software Usage Terms plus any specific additional conditions applying to this product.If you do not wish to accept these terms, do not download this product.See Terms of Service for more important information.',
-
-    },
-    ru: {
-        //header
-        editions: 'Издания',
-        controller: 'Джостик',
-        about: 'Об Игре',
-        explore: 'Исследовать',
-        news: 'Новости',
-        faq: 'Помощь',
-        release: 'До выпуска',
-        title: 'Бог войны Рагнарёк',
-        discriptionSubtitle: 'События, происходящие через три года после событий предыдущей игры, Fimbulwinter, великая зима, которая продлилась три лета, подходит к концу, что положит начало предсказанному Рагнареку',
-        discriptionBtn: 'купить',
-
-        //editions
-        editionsTitle: 'Стандартные издания',
-        editionsItemStandartFirst: 'Offline play enabled',
-        editionsItemStandartSecond: 'Remote Play supported',
-        editionsItemStandartLast: 'Trigger effect required',
-        editionsTitleDeluxe: 'Эксклюзивное издание',
-        editionsItemDeluxeFirst: 'All features of Standard Edition',
-        editionsItemDeluxeSecond: 'Darkdale Armour',
-        editionsItemDeluxetLast: 'Official digital soundtrack',
-
-        //controller
-        controllerTitle: 'DualSense',
-        controllerText: 'You can also buy an exclusive controller with the theme for PS5',
-        controllerOffer: 'If you purchase a controller and a game(any edition), you will receive a discount on two items',
-        controllerBuyBtn: 'buy dualsense with a game',
-
-        //video
-        sectionTitle: 'About Game',
-        videoTitle: 'Watch the Trailer',
-        videoBtn: 'play',
-
-        //explore
-        exploreTitle: 'Explore the God of War series',
+        exploreItem: 'Here are the God of War (PC) System Requirements',
         exploreswitchMinimum: 'Minimum',
         exploreswitchRecommended: 'Recommended',
         exploreswitchStandart: 'Standart',
@@ -125,6 +62,71 @@ export const languages = {
 
         //footer
         footerInfo: 'Download of this product is subject to the PlayStation Network Terms of Service and our Software Usage Terms plus any specific additional conditions applying to this product.If you do not wish to accept these terms, do not download this product.See Terms of Service for more important information.',
+
+    },
+    ru: {
+        //header
+        editions: 'Издания',
+        controller: 'Контроллер',
+        about: 'Об Игре',
+        explore: 'Исследовать',
+        news: 'Новости',
+        faq: 'Помощь',
+        release: 'До выпуска',
+        title: 'Бог войны Рагнарёк',
+        discriptionSubtitle: 'События, происходящие через три года после событий предыдущей игры, Fimbulwinter, великая зима, которая продлилась три лета, подходит к концу, что положит начало предсказанному Рагнареку',
+        discriptionBtn: 'купить',
+
+        //editions
+        editionsTitle: 'Стандартные издания',
+        editionsItemStandartFirst: 'Играй оффлайн',
+        editionsItemStandartSecond: 'Дистанционная игра',
+        editionsItemStandartLast: 'Необходим триггерный эффект',
+        editionsTitleDeluxe: 'Эксклюзивное издание',
+        editionsItemDeluxeFirst: 'Все особенности стандартного издания',
+        editionsItemDeluxeSecond: 'Доспехи Даркдейла',
+        editionsItemDeluxetLast: 'Официальный саундтрек',
+
+        //controller
+        controllerTitle: 'DualSense',
+        controllerText: 'Вы также можете купить эксклюзивный контроллер с темой для PS5',
+        controllerOffer: 'Если вы приобретете контроллер и игру (любое издание), вы получите скидку на два товара',
+        controllerBuyBtn: 'купить dualsense и игру',
+
+        //video
+        sectionTitle: 'Об Игре',
+        videoTitle: 'Смотреть трейллер',
+        videoBtn: 'Воспроизвести',
+
+        //explore
+        exploreTitle: 'Исследуйте серию "Бог войны"',
+        exploreItem: 'Системные требования God of War (ПК)',
+        exploreswitchMinimum: 'Минимум',
+        exploreswitchRecommended: 'Рекомендованно',
+        exploreswitchStandart: 'Стандарт',
+        exploreswitchLimited: 'Лимитированный',
+
+        //news
+        newsItemTitle: 'При релизе доступно большое обновление',
+        newsItemsubtitle: 'Долгожданная игра God of War: Ragnarok дебютирует чуть более чем через двадцать дней.По этому случаю представители студии Santa Monica совместно с Sony решили выпустить новый трейлер, посвященный эпической истории Кратоса и Атрея - таким образом, мы получили подтверждение того, что Рагнарек является заключительной главой приключений бога войны.',
+        newsBtn: 'читать',
+        newsItemTitleSecond: 'Официально является одной из самых больших игр для PS4 по размеру',
+        newsItemsubtitleSecond: 'Энтузиастам цифровой дистрибуции нужно быть готовыми к тоннам загрузок.Второе приключение Кратоса и Атрея на Крайнем Севере с момента премьеры займет почти 120 ГБ - при принятии решения о покупке игры в PlayStation Store стоит грамотно спланировать загрузку, чтобы запустить игру в день премьеры.',
+        newsItemTitleThird: 'Это заключительная глава в эпической истории. Смотрите новое видео',
+        newsItemSubtitleThird: 'В игру вернутся знакомые персонажи, но также и совершенно новые.Речь здесь идет о Торе, представленном как жестокий и психически неуравновешенный исполнитель "грязной работы" на службе у Одина, а также сам правитель богов, который оставил свой след в событиях предыдущей части, хотя и не появлялся в ней лично.',
+        newsItemTitleFourth: 'Рассказывает сложную историю',
+        newsItemSubtitleFourth: 'Долгожданная игра God of War: Ragnarok дебютирует чуть более чем через двадцать дней.По этому случаю представители студии Santa Monica совместно с Sony решили выпустить новый трейлер, посвященный эпической истории Кратоса и Атрея - таким образом, мы получили подтверждение того, что Рагнарек является заключительной главой приключений бога войны.',
+
+        //FAQ
+        faqItem: 'Когда выйдет следующий "Бог войны"?',
+        faqItemContent: 'God of War Ragnarök выйдет в  9 ноября 2022 года.Чтобы получать последние новости о сроках выпуска, обязательно следите за Santa Monica Studio в Twitter, Instagram и Facebook!',
+        faqItemSecond: 'Будут ли у Бога войны Рагнарека обновления?',
+        faqItemContentSecond: 'Да)',
+        faqItemThird: 'О чем серия игр Бог войны?',
+        faqItemContentThird: 'Первая эра (греческая мифология) God of War была впервые выпущена в Северной Америке 22 марта 2005 года для PlayStation 2. После десяти лет службы олимпийским богам спартанский солдат Кратос получает задание Афины найти ящик Пандоры, ключ к победе над Аресом, Богом войны, который буйствующий в Афинах.',
+
+        //footer
+        footerInfo: 'Загрузка этого продукта регулируется Условиями предоставления услуг PlayStation Network и нашими Условиями использования программного обеспечения, а также любыми конкретными дополнительными условиями, применимыми к этому продукту.Если вы не хотите принимать эти условия, не загружайте этот продукт.Смотрите Условия предоставления услуг для получения более важной информации.',
 
     },
 };
